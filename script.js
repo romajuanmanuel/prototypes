5e4d6fa61fd760e09f11ea106db8dd6f43daec6e
romaxolote <romajuanmanuel@gmail.com>
2025-06-06 13:51:08 -0300

    renderLibrary function created

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 /******************TEST BUTTON ***********/
 document.getElementById('btnSaludar').addEventListener('click', () => {
-    addBookToLibrary("hola","chau",1000,true);
-    addBookToLibrary("holasda","cdsdhau",8000,true);
-    addBookToLibrary("holasdasa","casdsahau",100,false);
+    addBookToLibrary("hola", "chau", 1000, true);
+    addBookToLibrary("holasda", "cdsdhau", 8000, true);
+    addBookToLibrary("holasdasa", "casdsahau", 100, false);
     console.log(library[0]);
     console.log(library[1]);
     console.log(library[2]);
+    renderLibraryCards(library);
 });
 
 const library = [];
@@ -25,10 +26,28 @@ class Book {
     }
 }
 
- function addBookToLibrary(title,author,pages,hasRead){
-    const newBook = new Book (crypto.randomUUID(),title,author,pages,hasRead);
+function addBookToLibrary(title, author, pages, hasRead) {
+    const newBook = new Book(crypto.randomUUID(), title, author, pages, hasRead);
     library.push(newBook);
- }
+}
+
+// Función para renderizar las tarjetas
+function renderLibraryCards(booksArray) {
+    const container = document.getElementById("library-container");
+
+    // Limpiar el contenedor por si ya tenía contenido
+    container.innerHTML = "";
+
+    booksArray.forEach(book => {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <h3>ID: ${book.id}</h3>
+        <p>Title: ${book.title}</p>
+      `;
+        container.appendChild(card);
+    });
+}
 
 
 
@@ -38,12 +57,5 @@ Book.prototype.greet = function () {
     return `${this.name} says hello.`;
 }
 
-
-const book1 = new Book("Juramento de sangre", "Brandon Sanderson", 1000, true);
-const book2 = new Book("El camino de los reyes", "Brandon Sanderson", 1200, false);
-const book3 = new Book("El aliento de los dioses", "Brandon Sanderson", 800, true);
-const book4 = new Book("El imperio final", "Brandon Sanderson", 500, false);
-const book5 = new Book("El héroe de las eras", "Brandon Sanderson", 600, true);
-const book6 = new Book("Palabras radiantes", "Brandon Sanderson", 700, false);
 
 Object.getPrototypeOf(book1) === Book.prototype;
